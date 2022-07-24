32a297737bd38de9db8c5a6e514f9c4036a0c133
Favik <75761451+Favik@users.noreply.github.com>
2022-07-24 21:29:28 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config = {}
 Config.ESXnotify = true                  -- If set to false mythic_notify is used for notifications and if set to true ESX notifications are used.
 
 Config.Item = 'depokit'                   -- Usable item
-Config.KeyBind = 38                       -- Build/fold depot and repair vehicle bind - https://docs.fivem.net/docs/game-references/controls/
+Config.KeyBind = 38                       -- Build/Dismantling depot and repair vehicle bind - https://docs.fivem.net/docs/game-references/controls/
 
 Config.BuildTime = 5                      -- Build depot in sec
 Config.FoldTime = 5                       -- Fold depot in sec
@@ -15,7 +15,7 @@ Config.Locale = 'en'                      -- Language settings / Translation
 
 Locales['cs'] = {
     ['repair_veh'] = 'Opravit vozidlo',
-    ['fold_depo'] = 'Slozit depo',
+    ['fold_depo'] = 'Demontáz depo',
     ['start_fix'] = 'Opravujete vozidlo',
     ['fixed'] = 'Vozidlo opraveno',
 }
@@ -29,7 +29,7 @@ Locales['pl'] = {
 
 Locales['en'] = {
     ['repair_veh'] = 'Repair the vehicle',
-    ['fold_depo'] = 'Fold the depot',
+    ['fold_depo'] = 'Dismantling the depot',
     ['start_fix'] = 'Repairing your vehicle',
     ['fixed'] = 'The vehicle is repaired',
 }
